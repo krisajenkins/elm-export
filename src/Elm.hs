2a0e578f93a57f7752ab4a705366cfd60911f49f
Kris Jenkins <krisajenkins@gmail.com>
2017-02-11 21:32:36 +0000

    Exporting Elm.Common.require, for the new moduleSpec code.

--- src/Elm.hs
+++ src/Elm.hs
@@ -2,7 +2,7 @@ module Elm
   ( module X
   ) where
 
-import Elm.Common as X (Options(..), defaultOptions)
+import Elm.Common as X (Options(..), defaultOptions, require)
 import Elm.Decoder as X
 import Elm.Encoder as X
 import Elm.File as X
