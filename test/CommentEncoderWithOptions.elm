d2cf67a12d304684469a2001f915e6cd4dc0f124
Micah Hahn <micah.s.hahn@gmail.com>
2022-09-21 09:38:55 -0400

    Fix encoder tests

--- test/CommentEncoderWithOptions.elm
+++ test/CommentEncoderWithOptions.elm
@@ -8,10 +8,10 @@ import Json.Encode
 encodeComment : Comment -> Json.Encode.Value
 encodeComment x =
     Json.Encode.object
-        [ ( "commentPostId", Json.Encode.int x.postId )
-        , ( "commentText", Json.Encode.string x.text )
-        , ( "commentMainCategories", (\(m0, n0) -> Json.Encode.list identity [ Json.Encode.string m0, Json.Encode.string n0 ]) x.mainCategories )
-        , ( "commentPublished", Json.Encode.bool x.published )
-        , ( "commentCreated", (Iso8601.encode) x.created )
-        , ( "commentTags", (Json.Encode.dict Json.Encode.string Json.Encode.int) x.tags )
+        [ ( "commentPostId", Json.Encode.int x.commentPostId )
+        , ( "commentText", Json.Encode.string x.commentText )
+        , ( "commentMainCategories", (\(m0, n0) -> Json.Encode.list identity [ Json.Encode.string m0, Json.Encode.string n0 ]) x.commentMainCategories )
+        , ( "commentPublished", Json.Encode.bool x.commentPublished )
+        , ( "commentCreated", (Iso8601.encode) x.commentCreated )
+        , ( "commentTags", (Json.Encode.dict Json.Encode.string Json.Encode.int) x.commentTags )
         ]
